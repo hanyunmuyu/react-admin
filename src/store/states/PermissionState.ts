fd6583ea171e893ee83ff7bbe7e69593cf201fc0
hanyun <1355081829@qq.com>
2021-01-25 15:34:25 +0800

    权限路由表设计

--- src/store/states/PermissionState.ts
+++ src/store/states/PermissionState.ts
@@ -1,16 +1,16 @@
 import {ReactNode} from "react";
 
 export interface IRoute {
-    id: string,
-    exact?: boolean
-    path: string
-    title: string
-    parentId?: number
-    isMenu?: number
-    component?: ReactNode
-    routes?: IRoute[]
-    redirect?: string
-    icon?: ReactNode
+    id: string,//唯一标识
+    exact?: boolean//是否精确匹配
+    path: string//路由地址
+    title: string//标题
+    parentId?: number//父级id
+    isMenu?: number//是否是菜单
+    component?: ReactNode//路由的内容主体，有于显示该条路由的内容
+    routes?: IRoute[]//子路由
+    redirect?: string//重定向地址
+    icon?: ReactNode//图标
 }
 
 export interface PermissionState {
